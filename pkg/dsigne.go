2d98602802b28c142ef2a60e8dbef4cc7ee8aeb1
AniMaro <m.kahlaouizakaria@gmail.com>
2024-09-07 23:24:12 +0100

    1.4

--- pkg/dsigne.go
+++ pkg/dsigne.go
@@ -7,38 +7,39 @@ import (
 func Start_Menu(Args []string) bool {
 	var start string
 
-	system := "windows" // linux or windows
+	system := "windows" // or can be "linux" based on the system
 
 	fmt.Println("***************************************")
 	fmt.Println("*             Go Reloaded             *")
 	fmt.Println("***************************************")
-	fmt.Printf(" [1] old file : %v\n", Args[1])
-	fmt.Printf(" [2] new file : %v\n", Args[2])
+	fmt.Printf(" [1] Old file: %v\n", Args[1])
+	fmt.Printf(" [2] New file: %v\n", Args[2])
 	fmt.Println("***************************************")
 
 	for {
-		fmt.Println("You want to start the process Y/N : ")
+		fmt.Println("Do you want to start the process? (Y/N): ")
 		fmt.Scan(&start)
-		if (start == "Y") || (start == "y") {
+		if start == "Y" || start == "y" {
 			Clear_Console(system)
 			return true
-		} else if (start == "N") || (start == "n") {
+		} else if start == "N" || start == "n" {
 			Clear_Console(system)
 			return false
 		} else {
-			fmt.Println("Please Enter the correct chois Y or N to continue")
+			fmt.Println("Please enter a valid choice Y or N to continue.")
 		}
 	}
 }
 
 func Modifications_Menu() int {
 	var choice int
-	system := "windows" // can be "linux" or "windows"
+	system := "windows" // or can be "linux" based on the system
 	Clear_Console(system)
 
 	fmt.Println("****************************************")
 	fmt.Println("*             Text Modifier            *")
 	fmt.Println("****************************************")
+	fmt.Println(" [0] Apply all modifications")
 	fmt.Println(" [1] Replace hexadecimal numbers (hex) with decimal")
 	fmt.Println(" [2] Replace binary numbers (bin) with decimal")
 	fmt.Println(" [3] Convert words to uppercase (up)")
@@ -46,19 +47,19 @@ func Modifications_Menu() int {
 	fmt.Println(" [5] Capitalize the first letter of words (cap)")
 	fmt.Println(" [6] Apply (low), (up), (cap) to a specified number of words")
 	fmt.Println(" [7] Adjust spaces between punctuation and words")
-	fmt.Println(" [8] Adjust quotes (' ') around words")
+	fmt.Println(" [8] Adjust single quotes (' ') around words")
 	fmt.Println(" [9] Replace 'a' with 'an' if the next word starts with a vowel or 'h'")
 	fmt.Println(" [10] Exit")
 	fmt.Println("****************************************")
 
 	for {
-		fmt.Println("Please select the modification you want to apply (1-10): ")
+		fmt.Println("Please select the modification you want to apply (0-10): ")
 		fmt.Scan(&choice)
-		if choice >= 1 && choice <= 10 {
+		if choice >= 0 && choice <= 10 {
 			Clear_Console(system)
 			return choice
 		} else {
-			fmt.Println("Invalid choice. Please enter a number between 1 and 10.")
+			fmt.Println("Invalid choice. Please enter a number between 0 and 10.")
 		}
 	}
 }
--- pkg/main_functions.go
+++ pkg/main_functions.go
@@ -31,46 +31,79 @@ func Read_File() (string, error) {
 
 func Modifications(Data string) (string, error) {
 	var err error
-
+	all := 0
 	for {
-		choice := Modifications_Menu()
+		var choice int
 
-		switch choice {
-		case 1:
-			fmt.Println("You selected: Replace hexadecimal numbers (hex) with decimal.")
+		if all == 0 {
+			choice = Modifications_Menu()
+			if choice == 0 {
+				all++
+			}
+		}
+		switch {
+		case choice == 1 || all == 1:
+			//("You selected: Replace hexadecimal numbers (hex) with decimal.")
 			Data, err = Convert_To(Data, "(hex)")
 			if err != nil {
 				return "", err
 			}
-		case 2:
-			fmt.Println("You selected: Replace binary numbers (bin) with decimal.")
+			if all != 0 {
+				all++
+			}
+		case choice == 2 || all == 2:
+			//("You selected: Replace binary numbers (bin) with decimal.")
 			Data, err = Convert_To(Data, "(bin)")
 			if err != nil {
 				return "", err
 			}
-		case 3:
-			fmt.Println("You selected: Convert words to uppercase (up).")
+			if all != 0 {
+				all++
+			}
+		case choice == 3 || all == 3:
+			//("You selected: Convert words to uppercase (up).")
 			Data, err = Low_Up_Cap(Data, "(up)")
-		case 4:
-			fmt.Println("You selected: Convert words to lowercase (low).")
+			if all != 0 {
+				all++
+			}
+		case choice == 4 || all == 4:
+			//("You selected: Convert words to lowercase (low).")
 			Data, err = Low_Up_Cap(Data, "(low)")
-		case 5:
-			fmt.Println("You selected: Capitalize the first letter of words (cap).")
+			if all != 0 {
+				all++
+			}
+		case choice == 5 || all == 5:
+			//("You selected: Capitalize the first letter of words (cap).")
 			Data, err = Low_Up_Cap(Data, "(cap)")
-		case 6:
-			fmt.Println("You selected: Apply (low), (up), (cap) to a specified number of words.")
+			if all != 0 {
+				all++
+			}
+		case choice == 6 || all == 6:
+			//("You selected: Apply (low), (up), (cap) to a specified number of words.")
 			Data, err = Low_Up_Cap_Specified(Data)
-
-		case 7:
-			fmt.Println("You selected: Adjust spaces between punctuation and words.")
+			if all != 0 {
+				all++
+			}
+		case choice == 7 || all == 7:
+			//("You selected: Adjust spaces between punctuation and words.")
 			Data = Punctuations(SplitPunctuations(Data))
-		case 8:
-			fmt.Println("You selected: Adjust quotes (' ') around words.")
+			if all != 0 {
+				all++
+			}
+		case choice == 8 || all == 8:
+			//("You selected: Adjust quotes (' ') around words.")
 			Data = Single_Cote(Data)
-		case 9:
-			fmt.Println("You selected: Replace 'a' with 'an' if the next word starts with a vowel or 'h'.")
+			if all != 0 {
+				all++
+			}
+		case choice == 9 || all == 9:
+			//("You selected: Replace 'a' with 'an' if the next word starts with a vowel or 'h'.")
 			// Call the function to replace 'a' with 'an'
-		case 10:
+
+			if all != 0 {
+				all++
+			}
+		case choice == 10 || all == 10:
 			fmt.Println("Exiting program.")
 			return Data, err
 		}
