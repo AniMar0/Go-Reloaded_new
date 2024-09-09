00cf0b28a51c5924d81052b71054f82f24f5c00e
Your Name <you@example.com>
2024-09-09 03:38:20 +0100

    Single cote modif

--- pkg/SingleCote.go
+++ pkg/SingleCote.go
@@ -6,27 +6,46 @@ func Single_Cote(content string) string {
 	if len(new_content) == 0 {
 		return content
 	}
-	colon := false
+
+	counter := 0
 	str := ""
-
-	for _, arg := range new_content {
-		// println(arg)
-		if arg == "'" && !colon {
-			colon = true
-		} else if arg == "'" && colon {
-			colon = false
+	for i, arg := range new_content {
+		
+		if arg == "'" {
+			counter++
 		}
 
-		if colon {
-			str += RemoveSpace(arg)
-		} else {
-			if arg[len(arg)-1] != ' ' {
-				str += arg + " "
-			} else {
-				str += arg
-			}
+		if counter == 2 {
+			str += "'" + RemoveSpace(new_content[i-1]) + "'"
+			counter = 0
+		} else if counter == 0 {
+			str += arg
+		} else if i == len(new_content)-1 && counter == 1 {
+			str += "'" + arg
 		}
+
 	}
+	// colon := false
+	// str := ""
+
+	// for _, arg := range new_content {
+	// 	// println(arg)
+	// 	if arg == "'" && !colon {
+	// 		colon = true
+	// 	} else if arg == "'" && colon {
+	// 		colon = false
+	// 	}
+
+	// 	if colon {
+	// 		str += RemoveSpace(arg)
+	// 	} else {
+	// 		if arg[len(arg)-1] != ' ' {
+	// 			str += arg + " "
+	// 		} else {
+	// 			str += arg
+	// 		}
+	// 	}
+	// }
 
 	return str
 }
